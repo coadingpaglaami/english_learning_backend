94b063290b57e3629d5f78c5a8f1c07e4a47407d
Mehedy Hasan  Piale <mehedyhasan4954@gmail.com>
2026-04-13 09:30:05 +0600

    feat: some changes

--- src/guards/guard.module.ts
+++ src/guards/guard.module.ts
@@ -1,9 +1,10 @@
 // guard.module.ts
 import { Module } from "@nestjs/common";
 import { GoogleRoleGuard } from "./google-role.guard";
+import { RolesGuard } from "./role.guard";
 
 @Module({
-  providers: [GoogleRoleGuard],
-  exports: [GoogleRoleGuard], // Export it so AuthModule can see it
+  providers: [GoogleRoleGuard,RolesGuard],
+  exports: [GoogleRoleGuard,RolesGuard], // Export it so AuthModule can see it
 })
 export class GuardModule {}
---
+++ src/guards/role.guard.ts
@@ -0,0 +1,24 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Role, Roles } from 'src/decorator/role.decorator';
+
+function matchRoles(requiredRoles: Role[], userRoles: Role[]): boolean {
+  return requiredRoles.some((role) => userRoles.includes(role));
+}
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+  canActivate(context: ExecutionContext): boolean {
+    const roles = this.reflector.get(Roles, context.getHandler());
+    if (!roles) return true;
+
+    const request = context.switchToHttp().getRequest();
+
+    const user = request.user;
+
+    console.log(user.role, 'role in the guard');
+
+    return matchRoles(roles, user.role);
+  }
+}
