adc738ad8bff873db06cf08d8afbf8a681963d10
Mehedy Hasan  Piale <mehedyhasan4954@gmail.com>
2026-04-20 01:52:01 +0600

    feat: done the question type works

--- src/guards/role.guard.ts
+++ src/guards/role.guard.ts
@@ -17,8 +17,6 @@ export class RolesGuard implements CanActivate {
 
     const user = request.user;
 
-    console.log(user.role, 'role in the guard');
-
     return matchRoles(roles, user.role);
   }
 }
